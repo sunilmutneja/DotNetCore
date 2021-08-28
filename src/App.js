175ecb7571840a745db1311546e4a61ab7542088
sunilmutneja <sunilmutneja@gmail.com>
2021-08-28 10:16:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     };
 
     componentDidMount() {
-        fetch('http://52.91.140.26/a.json')
+        fetch('http://52.91.140.26/restaurant/')
             .then(res => res.json())
             .then((data) => {
 				 console.log(data);
