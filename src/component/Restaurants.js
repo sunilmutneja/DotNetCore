5c8f6ab74dbd371c2d002ec559f3fdb774f0d08c
sunilmutneja <sunilmutneja@gmail.com>
2021-08-28 16:32:03 +0530

    Update Restaurants.js

--- src/component/Restaurants.js
+++ src/component/Restaurants.js
@@ -5,7 +5,33 @@ const RestaurantsDisplay = (props) => {
     console.log("Props is>>>>>",props)
 
     const renderList = ({datalist}) => {
-       
+        if(datalist){
+            return datalist.map((item,index) => {
+                return(
+                    <div className="card col-md-2">
+                            <div className="row">
+                            <img className="card-img-top" src={item.thumb} alt="thumb"/>
+                            <span className="topTemp">{item.name}</span>
+                            </div>
+                            <div className="card-body">
+                            <span className="max">{item.city}</span>   /
+                                      <span className="min">{item.locality}</span>
+                                <h4 className="card-title">2 Person = ₹{item.min_price}</h4>
+                                
+                            </div>
+                        </div>
+                )
+            })
+        }else{
+            return(
+                <div>
+                    <center>
+                    <img src="/loading.gif"/>
+                    </center>
+                </div>
+               
+            )
+        }
         
 
     }
