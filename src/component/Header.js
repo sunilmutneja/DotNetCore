1f9204291fe8626c7e52bba1df07ee4e51283a1a
sunilmutneja <sunilmutneja@gmail.com>
2021-08-28 16:10:00 +0530

    Add files via upload

--- src/component/Header.js
+++ src/component/Header.js
@@ -7,25 +7,25 @@ class Header extends Component {
       
         return(
             <Fragment>
-               <nav class"=navbar navbar-inverse">
-                <div class"=container-fluid">
-                    <div class"=navbar-header">
-                    <button type="button" class"=navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class"=icon-bar"></span>
-                        <span class"=icon-bar"></span>
-                        <span class"=icon-bar"></span>                        
+               <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
                     </button>
-                    <a class"=navbar-brand">Developer Funnel</a>
+                    <a class="navbar-brand">Developer Funnel</a>
                     </div>
-                    <div class"=collapse navbar-collapse" id="myNavbar">
-                    <ul class"=nav navbar-nav">
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
                         <li ><a >Home</a></li>
                         <li><a >Page 2</a></li>
                         <li><a>Page 3</a></li>
                     </ul>
-                    <ul class"=nav navbar-nav navbar-right">
-                        <li><a><span class"=glyphicon glyphicon-user"></span> Sign Up</a></li>
-                        <li><a ><span class"=glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a ><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                     </ul>
                     </div>
                 </div>
--- src/component/Home.js
+++ src/component/Home.js
@@ -2,7 +2,7 @@ import React, {Component, Fragment} from 'react';
 import Header from './Header';
 import RestaurantsDisplay from './Restaurants';
 
-const url = "http://52.91.140.26/restaurant/";
+const url = "http://52.91.140.26/restaurant";
 
 class Home extends Component{
     constructor(){
